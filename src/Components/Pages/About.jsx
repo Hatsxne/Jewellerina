522cedfb1aab26bfb655c951b0714c70af1c7700
Maisam Al-Nuaimi <malnu061@uottawa.ca>
2023-07-05 20:18:55 -0400

    updated 'About' page

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -5,7 +5,12 @@ function About() {
   return (
     <div className="container">
       <div className="content-bg"></div>
-      <div className="body">"Discover The Sparkle Within"</div>
+      <div className="body">"Welcome to Jewellerina, where exquisite craftsmanship meets timeless elegance. 
+      We pride ourselves on being a premier destination for discerning jewelry enthusiasts seeking the finest pieces that reflect their unique style and celebrate life's most precious moments. 
+      With a rich heritage of more than three decades, Jewellerina has established itself as a trusted name in the industry, renowned for our uncompromising commitment to quality, attention to detail, and exceptional customer service. 
+      Our curated collection showcases an exquisite array of diamond and gemstone jewelry, expertly crafted by our talented artisans who pour their passion and expertise into each creation. 
+      Whether you're looking for the perfect engagement ring, a memorable gift, or a piece to treasure for a lifetime, we invite you to explore our collection and experience the elegance and beauty that defines Jewellerina. 
+      Let us help you find the jewelry that tells your unique story and captures the essence of your most cherished moments."</div>
     </div>
   );
 }
